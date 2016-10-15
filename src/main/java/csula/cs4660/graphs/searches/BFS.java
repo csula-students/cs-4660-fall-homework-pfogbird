44b9e66136d215d6f84b36f0a32db8191b5fa1af
pfogbird <kparton510@gmail.com>
2016-10-15 12:53:27 -0700

    BFS

--- src/main/java/csula/cs4660/graphs/searches/BFS.java
+++ src/main/java/csula/cs4660/graphs/searches/BFS.java
@@ -4,17 +4,62 @@ import csula.cs4660.graphs.Edge;
 import csula.cs4660.graphs.Graph;
 import csula.cs4660.graphs.Node;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Breadth first search
  */
 public class BFS implements SearchStrategy {
+
     @Override
     public List<Edge> search(Graph graph, Node source, Node dist) {
 
+        Queue queue = new LinkedList<Node>();
+        List<Node> result = new ArrayList<>();
+        HashMap<Node, Node> nodeTracker = new HashMap<>();
+        List<Node> accumulator = new ArrayList<>();
+
+        Node end;
+
+        while(!queue.isEmpty()) {
+            Node current = (Node)queue.poll();
+
+            for (Node node: graph.neighbors(current)) {
+
+                if(!accumulator.contains(node)) {
+
+                    nodeTracker.put(node, current);
+
+                    if (graph.equals(dist)) {
+
+                        end = new Node(node.getData());
+
+                    }
+                    else if (!result.contains(node)) {
+
+                        queue.add(node);
+                        result.add(node);
+                        nodeTracker.put(node, current);
+
+                    }
+                    queue.add(node);
+                    accumulator.add(node);
+
+
+                }
+
+            }
+
+        }
+
+        while () {
+
+
+
+        }
 
 
         return null;
     }
+
 }
