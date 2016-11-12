757cd05184aab9959401c83434b3f4442a9f58bb
pfogbird <kparton510@gmail.com>
2016-11-12 09:32:35 -0800

    Merge https://github.com/csula/cs4660-fall-2016
    # Conflicts:
    #	build.gradle
    #	src/main/java/csula/cs4660/graphs/Graph.java
    #	src/main/java/csula/cs4660/graphs/representations/AdjacencyMatrix.java

--- src/main/java/csula/cs4660/graphs/representations/ObjectOriented.java
+++ src/main/java/csula/cs4660/graphs/representations/ObjectOriented.java
@@ -1,151 +1,114 @@
 package csula.cs4660.graphs.representations;
 
+import com.google.common.collect.Lists;
 import csula.cs4660.graphs.Edge;
 import csula.cs4660.graphs.Node;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Object oriented representation of graph is using OOP approach to store nodes
  * and edges
  */
 public class ObjectOriented implements Representation {
-
     private List<Node> nodes;
     private List<Edge> edges;
 
-
+    protected ObjectOriented() {
+        nodes = Lists.newArrayList();
+        edges = Lists.newArrayList();
+    }
     protected ObjectOriented(File file) {
-
-        nodes = new ArrayList<>();
-        edges = new ArrayList<>();
+        nodes = Lists.newArrayList();
+        edges = Lists.newArrayList();
 
         try {
-            Scanner scanner = new Scanner(file);
+            Scanner in = new Scanner(file);
 
-            while (scanner.hasNextLine()) {
+            // to skip the first line
+            in.nextLine();
 
-                String line = scanner.nextLine();
-                String [] members = line.split(":");
-                int weight = Integer.parseInt(members[2]);
+            // reading edges line by line
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+                int[] parts = Arrays.stream(
+                        line.split(":")
+                ).mapToInt(Integer::parseInt)
+                        .toArray();
+                Edge edge = new Edge(
+                        new Node<>(parts[0]),
+                        new Node<>(parts[1]),
+                        parts[2]
+                );
 
-                Node fromNode = new Node(Integer.parseInt(members[0]));
-                Node toNode = new Node(Integer.parseInt(members[1]));
-
-
-                nodes.add(fromNode);
-                nodes.add(toNode);
-
+                addEdge(edge);
             }
         } catch (FileNotFoundException e) {
-
-            System.out.println("There was an error.");
-
+            e.printStackTrace();
         }
-
-    }
-
-    protected ObjectOriented() {
-
-        nodes = new ArrayList<>();
-        edges = new ArrayList<>();
-
-    private Collection<Node> nodes;
-    private Collection<Edge> edges;
-
-    public ObjectOriented(File file) {
-    }
-
-    public ObjectOriented() {
-
-
     }
 
     @Override
     public boolean adjacent(Node x, Node y) {
-
-
-        for(Edge edge: edges){
-            if(edge.getFrom().equals(x) && edge.getTo().equals(y)){
+        for (Edge edge: edges) {
+            if (edge.getFrom().equals(x) && edge.getTo().equals(y)) {
                 return true;
             }
         }
-
-
         return false;
     }
 
     @Override
     public List<Node> neighbors(Node x) {
+        List<Node> result = Lists.newArrayList();
 
-
-        ArrayList<Node> neighborNodes = new ArrayList<>();
-
-        for (Edge e: edges) {
-
-            neighborNodes.add(e.getTo());
-
+        if (!nodes.contains(x)) {
+            return result;
         }
 
-        return neighborNodes;
+        result.addAll(edges.stream()
+                .filter(edge -> edge.getFrom().equals(x))
+                .map(Edge::getTo).collect(Collectors.toList()));
 
-        return null;
-
+        return result;
     }
 
     @Override
     public boolean addNode(Node x) {
-
-
-        if(nodes.contains(x)){
+        if (nodes.contains(x)) {
             return false;
         }
 
         nodes.add(x);
-
         return true;
-
     }
 
     @Override
     public boolean removeNode(Node x) {
-
-        if(!nodes.contains(x)){
+        if (!nodes.contains(x)) {
             return false;
         }
 
-        for (Edge e: edges) {
-
-            if (e.getTo().equals(x) || e.getFrom().equals(x)) {
-                edges.remove(x);
+        Iterator<Edge> iterator = edges.iterator();
+        while (iterator.hasNext()) {
+            Edge edge = iterator.next();
+            if (edge.getTo().equals(x) || edge.getFrom().equals(x)) {
+                iterator.remove();
             }
-
         }
 
         return nodes.remove(x);
-
-        return false;
-
     }
 
     @Override
     public boolean addEdge(Edge x) {
-
-
-        if(!nodes.contains(x.getFrom())){
+        if (!nodes.contains(x.getFrom())) {
             nodes.add(x.getFrom());
         }
-
-        if(!nodes.contains(x.getTo())){
+        if (!nodes.contains(x.getTo())) {
             nodes.add(x.getTo());
         }
 
@@ -155,45 +118,21 @@ public class ObjectOriented implements Representation {
 
         edges.add(x);
 
-       return true;
-
-        return false;
-
+        return true;
     }
 
     @Override
     public boolean removeEdge(Edge x) {
-
-
-
-
         return edges.remove(x);
     }
 
     @Override
     public int distance(Node from, Node to) {
-
-
-        int distance = 0;
-
-        for (Edge e: edges) {
-            if (e.getFrom().equals(from) && e.getTo().equals(to)) {
-                distance = e.getValue();
-                return distance;
+        for (Edge edge: edges) {
+            if (edge.getFrom().equals(from) && edge.getTo().equals(to)) {
+                return edge.getValue();
             }
         }
-
-        return distance;
-}
-
-    @Override
-    public Optional<Node> getNode(int index) {
-
-
-
-        return null;
-    }
-}
 
         return 0;
     }
@@ -205,15 +144,11 @@ public class ObjectOriented implements Representation {
 
     @Override
     public Optional<Node> getNode(Node node) {
-        Iterator<Node> iterator = nodes.iterator();
-        Optional<Node> result = Optional.empty();
-        while (iterator.hasNext()) {
-            Node next = iterator.next();
-            if (next.equals(node)) {
-                result = Optional.of(next);
-            }
-        }
-        return result;
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "";
     }
 }
-
